194673a62a9c09cc3bcf5830dc25713fc6ae974d
Rich Steinschneider <rcsteins@gmail.com>
2012-02-06 00:30:55 -0500

    throttle FILE added

---
+++ throttler.rb
@@ -0,0 +1,13 @@
+class Throttler
+  def initialize lim
+    @limit = lim
+    @throttle = -1
+  end
+  
+  def act &block
+    @throttle+=1
+    @throttle = 0  if @throttle > @limit
+    block.call if @throttle == 0
+    puts 'throttle '+ @throttle.to_s
+  end
+end
