2085c264f9ab5b59c5401bba24674e3d197e0821
Rich Steinschneider <rcsteins@gmail.com>
2012-02-11 17:45:10 -0500

    more dev on FreeList

--- mem_pool_t.rb
+++ mem_pool_t.rb
@@ -14,9 +14,10 @@ class FreeList
     end
   end
   
- 
+  #constuctor called on item must set item to valid disabled state
   def add_with_id item
     id = @id_gen.gen
+    #here we automatically mix in the IdManaged protocol
     item.extend IdManaged
     item.id_manager = self
     item.id =id
@@ -32,11 +33,15 @@ class FreeList
     nil
   end
   
+  #this returns valid disabled item
   def next_free
-    #need to increment before returning value
+    #need to increment before returning value, because returning exits function
     @next_free+=1
+    
+    #this shouldn't be in final class, caller must call re_init on returned object to 'activate'
     #return @next_free -1 because of the pre-incrementation
     @arr[@next_free-1].v=1
+    
     return @arr[@next_free-1]
   end
   
@@ -47,7 +52,7 @@ class FreeList
       @next_free-=1
       
       #for development, marking released items as -5
-      @arr[release_id].v = -5
+      return_to_disabled @arr[release_id]
       
       #swap ids
       @arr[@next_free].id,@arr[release_id].id = @arr[release_id].id,@arr[@next_free].id
@@ -56,6 +61,10 @@ class FreeList
       @arr[@next_free],@arr[release_id] = @arr[release_id],@arr[@next_free]
       nil
     end
+  end
+  
+  def return_to_disabled item
+    item.v= -5
   end
   
 end
