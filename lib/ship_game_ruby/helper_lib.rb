a4b4054c11c04064e4d453462dc22d72b216cde5
Rich Steinschneider <rcsteins@gmail.com>
2012-05-18 18:50:03 -0400

    removed throttler file, put in helper lib

--- lib/ship_game_ruby/helper_lib.rb
+++ lib/ship_game_ruby/helper_lib.rb
@@ -49,4 +49,17 @@ class SharedNum
   end
 end
 
+class Throttler
+  def initialize lim
+    @limit = lim
+    @throttle = -1
+  end
+  
+  def act &block
+    @throttle+=1
+    @throttle = 0  if @throttle > @limit
+    block.call if @throttle == 0
+  end
+end
+
 
