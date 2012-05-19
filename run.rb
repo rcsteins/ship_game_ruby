bd98cbb778c66279ad730e6ca46637748933b0e3
Rich Steinschneider <rcsteins@gmail.com>
2012-05-19 18:14:04 -0400

    restructuring of project to prepare for jruby comptatibility

--- run.rb
+++ run.rb
@@ -1,5 +1,16 @@
 $: << "."
-require 'lib/ship_game_ruby'
+require 'rubygems'
+require "bundler/setup"
 
-window = GameWindow.new
-window.show
+#require all gems here.  since we are using bundler, we know
+#they are here
+require 'gosu'
+#require all of your application files here
+root_directory = File.join(File.dirname(__FILE__), 'source')
+Dir["#{root_directory}/*.rb"].each {|file| require file }
+
+if __FILE__ == $0
+  window = GameWindow.new
+  window.show
+end
+
