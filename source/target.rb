7ab6974121c13db489b4601da3ccc807809e7a4a
Rich Steinschneider <rcsteins@gmail.com>
2012-06-08 21:53:27 -0400

    fix spelling error

--- source/target.rb
+++ source/target.rb
@@ -1,10 +1,13 @@
+$: << '.'
+require 'source/a_util_classes.rb'
+
 class Target
   include Drawable
   
   def initialize x,y,image
     @loc = Coors.new(x,y)
     @image = image
-    @angle = Sharednum.new(0)
+    @angle = SharedNum.new(0)
     @z = 1
   end
   
