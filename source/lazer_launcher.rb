3989731c05a942a5684ed4de748e11c52a74c456
Rich Steinschneider <rcsteins@gmail.com>
2012-06-09 20:17:34 -0400

    rename files

---
+++ source/lazer_launcher.rb
@@ -0,0 +1,25 @@
+#GOSU FREE
+class BulletBuilder
+  attr_accessor :speed,:throttling
+  
+  def initialize ship, free_list, active_list
+     @ship = ship
+     @body= ship.body
+     @free_list = free_list
+     @speed = 545
+     @throttling = 4
+     @count = -1
+     @active_list = active_list
+  end
+
+  def create 
+    @active_list << @free_list.next_free.re_init(@body.loc,@ship.aim_angle,@speed,:red)
+  end
+  
+  def throttled_create
+    @count += 1
+    @count = 0  if @count > @throttling
+    create if @count == 0
+  end
+
+end
