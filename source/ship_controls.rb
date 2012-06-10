10fa246f29582e21a57d655d947c2b9f61ae06d3
Rich Steinschneider <rcsteins@gmail.com>
2012-06-10 13:15:24 -0400

    consolidate source files

--- source/ship_controls.rb
+++ source/ship_controls.rb
@@ -43,3 +43,20 @@ class ShipControlDrivers
   end
   
 end
+
+class AngleReader
+  def initialize(body)
+    @body = body
+  end
+  
+  def read_data dest
+    abs = read_abs(dest)
+    diff = GLib.angle_diff(@body.angle,abs)
+    return abs,diff
+  end
+  private
+  def read_abs dest
+    src = @body.loc
+    return GLib.angle(src.x,src.y,dest.x,dest.y)
+  end 
+end
