3189011130ee936814904c6c5c5d8e74377ac893
Rich Steinschneider <rcsteins@gmail.com>
2012-06-09 20:33:26 -0400

    expand on new class

--- source/ship_basic_control.rb
+++ source/ship_basic_control.rb
@@ -1,8 +1,17 @@
 class ShipBasicControl
-  def initialize 
-    @aim_angle
-    @movement_angle
-    @move_target
-    @aim_target
+  attr_accessor :aim_angle,:aim_target, :move_angle,:move_target
+  def initialize params = {}
+    @aim_angle =  params(:aim_angle)
+    @aim_target = params(:aim_target)
+    @move_angle = params(:move_angle)
+    @move_target =params(:move_target)
+  end
+  
+  def set_both_angles v
+    @aim_angle,@move_angle = v,v
+  end
+  
+  def set_both_targets v
+    @aim_target,@move_target = v,v
   end
 end
