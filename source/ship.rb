8b81570a3292ad1e4c162868f9b57fbff0fae0eb
Rich Steinschneider <rcsteins@gmail.com>
2012-06-09 20:21:25 -0400

    code cleanup

--- source/ship.rb
+++ source/ship.rb
@@ -8,13 +8,13 @@ class Ship
   def initialize(img, options_in = {}) 
     options = {:x => 0,:y => 0, :angle => 0 , :team => :red, :turn => 300}.merge!(options_in)
     x,y = options[:x],options[:y]
+    @signal_handler = InputSignalHandler.new
     @body = GameBody.new(:x => x,:y => y, :angle => options[:angle],:image=>img,:vel => Coors.new(0,0),:ship=>self) 
     @my_engine = ShipEngine.new @body, :turn => options[:turn]
     @t_1 = 1.0
     @aim_angle = 0
+    @diff = 0
     @team = options[:team]
-    @diff = 0 
-    @signal_handler = InputSignalHandler.new
   end
   
   def add_launcher pool, active_list
@@ -33,7 +33,6 @@ class Ship
   
   def think
     @aim_angle,@diff = @angle_reader.read_data if @angle_reader
-
     amt = @t_1 * @diff/(@diff.abs+0.001)
     @signal_handler.rotate(amt)
   end
