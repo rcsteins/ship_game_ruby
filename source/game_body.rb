fcd17d5544d943229c53f033ff2cbe1d5412dfdc
Rich Steinschneider <rcsteins@gmail.com>
2012-06-09 20:04:07 -0400

    more transformations for body refactoring

--- source/game_body.rb
+++ source/game_body.rb
@@ -1,8 +1,8 @@
 class GameBody
   attr_accessor :loc,:angle,:image,:z,:vel
   def initialize params_in ={}
-    params = {:x =>0.0, :y => 0.0}
-    params.merge! params_in
+    defaults = {:x =>0.0, :y => 0.0}
+    params=defaults.merge(params_in)
     x,y = params[:x],params[:y]
     @loc = Coors.new(x,y)
     @angle = params[:angle]
