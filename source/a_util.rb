ea2344e76590c90be3e8eb2c726c0364bf5a4e7d
Rich Steinschneider <rcsteins@gmail.com>
2012-06-10 13:05:24 -0400

    consolidate files

--- source/a_util.rb
+++ source/a_util.rb
@@ -71,5 +71,13 @@ module StubMethods
   end
 end
 
-
+class Target
+  include Drawable
+  
+  def initialize x,y,image
+    @body = GameBody.new(:x => x, :y => y,:image =>image)
+    @z = 1
+  end
+  def update;end
+end
 
