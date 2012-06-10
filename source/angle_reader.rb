c6f5fe84cd2b8622b205e923549bb86ad2e21bd4
Rich Steinschneider <rcsteins@gmail.com>
2012-06-10 12:55:16 -0400

    small refactoring

--- source/angle_reader.rb
+++ source/angle_reader.rb
@@ -4,21 +4,14 @@ class AngleReader
     @body = body
   end
   
-  def read_abs dest
-    src = @body.loc
-    return GLib.angle(src.x,src.y,dest.x,dest.y)
-  end
-
-  def read_diff dest
-    abs = read_abs dest
-    diff = GLib.angle_diff(@body.angle,abs)
-    return diff
-  end
-  
   def read_data dest
-    abs = read_abs dest
+    abs = read_abs(dest)
     diff = GLib.angle_diff(@body.angle,abs)
     return abs,diff
   end
-    
+  private
+  def read_abs dest
+    src = @body.loc
+    return GLib.angle(src.x,src.y,dest.x,dest.y)
+  end 
 end
